290559490366d4c2ebe433bbee60dcac8b294880
kim kiamco <shayna.gaulden@gmail.com>
2022-06-15 15:31:45 -0700

    Working state

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "../components/nav";
+
+const Home = () => {
+    return(
+        <div>
+            <Nav/>
+            Hello
+        </div>
+    )
+}
+
+export default Home;
