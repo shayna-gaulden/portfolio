77f0adda9698b188d18428df0b05fc2e36108535
kim kiamco <shayna.gaulden@gmail.com>
2022-06-16 11:32:05 -0700

    uploaded photos

--- src/pages/home.js
+++ src/pages/home.js
@@ -8,8 +8,13 @@ const Home = () => {
             <Nav/>
             <Title
                 title={"Shayna Gaulden"}
-                subtitle={"Soon to be: Master in Data Science at San Jose State University"}
+                subtitle={"2023 Master in Data Science at San Jose State University"}
             />
+            <div className="title-container">
+                <header className="title">
+                Hey there! I’m a student studying Data Science and Statistics in San Jose, CA. Check out my projects below!
+                </header>
+            </div>
         </div>
     )
 }
