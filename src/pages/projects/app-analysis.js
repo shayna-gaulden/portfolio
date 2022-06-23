67144a446a3a68b0b4da3ada07e08d9fb43d7b9d
kim kiamco <shayna.gaulden@gmail.com>
2022-06-23 16:14:23 -0700

    updating latest project page

--- src/pages/projects/app-analysis.js
+++ src/pages/projects/app-analysis.js
@@ -36,8 +36,23 @@ const AppAnalysis = () => {
                 a Computer Science graduate at Carnegie Mellon University.
                 <br/><br/>
                 There are originally 10841 instances (applications) and 13 variables.
-                <li>hello</li>
-                <header className="mid-subtitle">hello</header>
+                <br/><br/>
+                <header className="mid-subtitle">Variables Used in Analysis</header>
+                <li>Rating: The average user rating, (scale from 0-5).</li>
+                <li>Reviews: The number of user reviews for the app.</li>
+                <li>Size: The size of the application in MB and KB.</li>
+                <li>Installs: The number of user downloads/installations for the app.</li>
+                <li>Price: The price of the application.</li>
+                <li>Category: Category that the app belongs to.</li>
+                <li>Type: Specifies whether the app is a free or a paid application.</li>
+                <li>Content Rating: Age group the app is targeted at.</li>
+                <li>Genre: The genre for an app can belong to multiple genres apart from its category.</li>
+
+                <header className="mid-title">Data Cleaning</header>
+                <header className="mid-subtitle">Missing Values</header>
+                All applications with missing values were removed from analysis, there were 1474 apps with missing ratings, 1 with missing reviews, and 1 missing a content rating.
+                <header className="mid-subtitle">Invalid Entries</header>
+                Removing any application whose size is not a number but is listed as 'varies with device'.
             </div>
 
             <Footer />
