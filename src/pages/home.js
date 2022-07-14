22298928ccc02a05a6531105c9455eaaffc65519
kim kiamco <shayna.gaulden@gmail.com>
2022-07-14 12:18:06 -0700

    chaging home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,6 +4,8 @@ import Title from "../components/title";
 import ProjPreview from "../components/proj-preview";
 import { ProjectData } from "../assets/projdata";
 import Footer from "../components/footer";
+import Resume from "../assets/images/Shayna Gaulden resume.pdf"
+
 
 const Home = () => {
     return(
@@ -14,16 +16,24 @@ const Home = () => {
                 title={"Shayna Gaulden"}
                 subtitle={"2023 Master in Data Science at San Jose State University"}
             />
+            <a href={Resume} target="_blank" rel="noreferrer" className="github-link">
+                    Download PDF Resume.
+            </a>
             <div className="title-container">
                 <header className="mid-title">
-                Hey there! I’m a student studying Data Science and Statistics in San Jose, CA. Check out my projects below!
+                Hey there! I’m a graduate student studying Data Science and Statistics in San Jose, CA. Check out my projects below!
                 </header>
             </div>
 
-            <div className="column">
-                
-            </div>
 
+
+            <div className="margin-cont">
+                <text className="home-title">
+                    Projects
+                </text>
+                <hr className="basic"/>
+            </div>
+            
             <div className="projects-container">
             {
                 ProjectData.map(el => {
